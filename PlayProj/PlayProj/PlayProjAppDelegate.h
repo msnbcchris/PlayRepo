81c02ed2ba594f619dc2b5bfb4eb671cac92a5cf
Chris Setter <chris.setter@msnbc.com>
2012-01-26 11:07:02 -0800

    added project

---
+++ PlayProj/PlayProj/PlayProjAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PlayProjAppDelegate.h
+//  PlayProj
+//
+//  Created by j-chrset on 1/26/12.
+//  Copyright (c) 2012 msnbc.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayProjAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlayProj/PlayProj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlayProj
+//
+//  Created by j-chrset on 1/26/12.
+//  Copyright (c) 2012 msnbc.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PlayProjAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PlayProjAppDelegate class]));
+    }
+}
